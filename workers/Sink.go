a3d5902b23f229f54cbc9334916ada0cb5a82d43
jelathro <jelathrop@gmail.com>
2014-07-14 19:30:36 -0400

    Initial commit of cluster boilerplate working.

---
+++ workers/Sink.go
@@ -0,0 +1,60 @@
+package workers
+
+import (
+	"emd/connector"
+	"emd/log"
+	"emd/worker"
+	"encoding/gob"
+)
+
+type Sink struct {
+	worker.Work
+}
+
+func (w Sink) Init() {
+	for _, p := range w.Ports() {
+		p.Open()
+	}
+
+	// Set the ExternalIngress type
+	gob.Register(new(Tuple))
+	w.Ports()["Sink_and_Uppercase"].(*connector.ExternalUDPIngress).Buf = new(Tuple)
+
+	log.INFO.Println("Worker " + w.Name + " inited.")
+}
+
+func (w Sink) Run() {
+	log.INFO.Println("Sink is running.")
+
+	// Catch any errors that could happen
+	defer func() {
+		if r := recover(); r != nil {
+			log.ERROR.Println("Uncaught error occurred, notifying leader and exiting.")
+
+			w.Stop()
+		}
+	}()
+
+	for {
+		select {
+		case cmd := <-w.Ports()["MGMT_Sink"].Channel():
+			if cmd == "STOP" {
+				w.Stop()
+				return
+			} else if cmd == "STATUS" {
+				w.Ports()["MGMT_Sink"].Channel() <- "Healthy"
+			} else if cmd == "METRICS" {
+				w.Ports()["MGMT_Sink"].Channel() <- Metric{"name": "value"}
+			}
+		case data := <-w.Ports()["Sink_and_Uppercase"].Channel():
+			log.INFO.Println(data)
+		}
+	}
+}
+
+func (w Sink) Stop() {
+	w.Ports()["MGMT_Sink"].Close()
+	w.Ports()["Sink_and_Uppercase"].Close()
+
+	log.INFO.Println("Worker " + w.Name + " stopped.")
+}
